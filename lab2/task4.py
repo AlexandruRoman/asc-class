bf6014c264b12ef39f9879ea8d64a98d203f4756
Alexandru Roman <alexroman5g@gmail.com>
2018-02-28 12:00:03 +0200

    task 4 - lab 2

--- lab2/task4.py
+++ lab2/task4.py
@@ -1,30 +1,13 @@
 import sys
-import threading
+from threading import *
 import time
-
-class Semaphore(object):
-
-    def __init__(self, initial):
-        self.lock = threading.Condition(threading.Lock())
-        self.value = initial
-
-    def up(self):
-        with self.lock:
-            self.value += 1
-            self.lock.notify()
-
-    def down(self):
-        with self.lock:
-            while self.value == 0:
-                self.lock.wait()
-            self.value -= 1
 
 class ChopStick(object):
 
     def __init__(self, number):
         self.number = number           # chop stick ID
         self.user = -1                 # keep track of philosopher using it
-        self.lock = threading.Condition(threading.Lock())
+        self.lock = Condition(Lock())
         self.taken = False
 
     def take(self, user):         # used for synchronization
@@ -46,26 +29,25 @@ class ChopStick(object):
             self.lock.notifyAll()
 
 
-class Philosopher (threading.Thread):
+class Philosopher (Thread):
 
     def __init__(self, number, left, right, butler):
-        threading.Thread.__init__(self)
+        Thread.__init__(self)
         self.number = number            # philosopher number
         self.left = left
         self.right = right
         self.butler = butler
 
     def run(self):
-        for i in range(20):
-            self.butler.down()              # start service by butler
-            time.sleep(0.1)                 # think
-            self.left.take(self.number)     # pickup left chopstick
-            time.sleep(0.1)                 # (yield makes deadlock more likely)
-            self.right.take(self.number)    # pickup right chopstick
-            time.sleep(0.1)                 # eat
-            self.right.drop(self.number)    # drop right chopstick
-            self.left.drop(self.number)     # drop left chopstick
-            self.butler.up()                # end service by butler
+        self.butler.acquire()              # start service by butler
+        time.sleep(0.1)                 # think
+        self.left.take(self.number)     # pickup left chopstick
+        time.sleep(0.1)                 # (yield makes deadlock more likely)
+        self.right.take(self.number)    # pickup right chopstick
+        time.sleep(0.1)                 # eat
+        self.right.drop(self.number)    # drop right chopstick
+        self.left.drop(self.number)     # drop left chopstick
+        self.butler.release()                # end service by butler
         sys.stdout.write("p[%s] finished thinking and eating\n" % self.number)
 
 
@@ -74,7 +56,7 @@ def main():
     n = 5
 
     # butler for deadlock avoidance (n-1 available)
-    butler = Semaphore(n-1)
+    butler = Semaphore(value = n-1)
 
     # list of chopsticks
     c = [ChopStick(i) for i in range(n)]
